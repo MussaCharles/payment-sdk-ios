50d67a05c04d1dc323ac8ed86683a7ecc57a460c
MussaCharles <mussacharles50@gmail.com>
2023-04-18 16:14:57 +0900

    Inject widget.renderAgreement(..) javascript method,observe height value of newly added agreement div,broadcast changes to parent controller

--- Sources/TossPayments/UIKit/Widget/PaymentWidget.swift
+++ Sources/TossPayments/UIKit/Widget/PaymentWidget.swift
@@ -9,6 +9,9 @@ import Foundation
 import WebKit
 
 public final class PaymentWidget: WKWebView, HandleURLResult {
+    
+    
+    // MARK: - Properties
     private var amount: Double = 0 {
         didSet {
             guard amount != oldValue else { return }
@@ -42,6 +45,33 @@ public final class PaymentWidget: WKWebView, HandleURLResult {
         return url
     }
     
+    private lazy var initializeWidgetScript = WKUserScript(
+        source: source,
+        injectionTime: .atDocumentEnd,
+        forMainFrameOnly: true
+    )
+    
+    private lazy var source: String = {
+        if let redirectURLString = self.options?.brandPay?.redirectURL {
+            return """
+                const widget = PaymentWidget("\(self.clientKey)", "\(self.customerKey)", {
+                  brandpay: { redirectUrl: "\(redirectURLString)" }
+                });
+                const { updateAmount } = widget.renderPaymentMethods('#payment-method', \(amount));
+                widget.renderAgreement('#agreement');
+                """
+        } else {
+            return """
+                const widget = PaymentWidget("\(self.clientKey)", "\(self.customerKey)", {
+                  brandpay: { redirectUrl: "" }
+                });
+                const { updateAmount } = widget.renderPaymentMethods('#payment-method', \(amount));
+                widget.renderAgreement('#agreement');
+                """
+        }
+    }()
+    
+    // MARK: - Life Cycle
     public init(
         clientKey: String,
         customerKey: String,
@@ -52,6 +82,7 @@ public final class PaymentWidget: WKWebView, HandleURLResult {
         self.options = options
         let configuration = WKWebViewConfiguration()
         super.init(frame: .zero, configuration: configuration)
+        bindContentSize()
     }
     
     public func renderPaymentMethods(amount: Double) {
@@ -60,7 +91,7 @@ public final class PaymentWidget: WKWebView, HandleURLResult {
         configuration.userContentController.add(RequestPaymentsMessageHandler(self), name: ScriptName.requestPayments.rawValue)
         configuration.userContentController.add(UpdateHeightMessageHandler(), name: ScriptName.updateHeight.rawValue)
         configuration.userContentController.add(RequestHTMLMessageHandler(self), name: ScriptName.requestHTML.rawValue)
-        loadHTMLString(htmlString, baseURL: baseURL)
+        loadHTMLString(self.htmlToLoadString(shouldIncludeLicense: true), baseURL: baseURL)
     }
     
     public func updateAmount(_ amount: Double) {
@@ -76,29 +107,6 @@ public final class PaymentWidget: WKWebView, HandleURLResult {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private lazy var source: String = {
-        if let redirectURLString = self.options?.brandPay?.redirectURL {
-            return """
-                const widget = PaymentWidget("\(self.clientKey)", "\(self.customerKey)", {
-                  brandpay: { redirectUrl: "\(redirectURLString)" }
-                });
-                const { updateAmount } = widget.renderPaymentMethods('#payment-method', \(amount));
-                """
-        } else {
-            return """
-                const widget = PaymentWidget("\(self.clientKey)", "\(self.customerKey)", {
-                  brandpay: { redirectUrl: "" }
-                });
-                const { updateAmount } = widget.renderPaymentMethods('#payment-method', \(amount));
-                """
-        }
-    }()
-    private lazy var initializeWidgetScript = WKUserScript(
-        source: source,
-        injectionTime: .atDocumentEnd,
-        forMainFrameOnly: true
-    )
     
     public func requestPayment(
         info: WidgetPaymentInfo,
@@ -119,11 +127,28 @@ public final class PaymentWidget: WKWebView, HandleURLResult {
             var script = decodeURIComponent(window.atob('\(encodedScript)'));
             eval(script);
             """) { (_, error) in
-                    
+                
             }
     }
     
-    private var htmlString: String {
+}
+
+
+// MARK: - HTML String
+
+private extension PaymentWidget {
+    
+    func htmlToLoadString(shouldIncludeLicense: Bool) -> String {
+        if shouldIncludeLicense {
+            return htmlStringWithLicenseSectionAdded
+        } else {
+            return originalHtmlString
+        }
+    }
+    
+    
+    /// Returns Original HTML string from Toss
+    var originalHtmlString: String {
         """
         <!DOCTYPE html>
         <html>
@@ -138,4 +163,68 @@ public final class PaymentWidget: WKWebView, HandleURLResult {
         </html>
         """
     }
+    
+    
+    /// Returns HTML String with an added agreement div (`<div id="agreement"></div>`).
+    var htmlStringWithLicenseSectionAdded: String {
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>결제하기</title>
+          <script src="https://js.tosspayments.com/\(TossPaymentsEnvironment.stage)/payment-widget"></script>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        </head>
+        <body style="margin:0;padding:0;overflow:hidden;">
+            <div id="payment-method"></div>
+            <div id="agreement"></div>
+        </body>
+        </html>
+        """
+    }
+    
 }
+
+
+// MARK: - New Agreement section logic
+
+extension PaymentWidget {
+    
+    /// A custom method which compute height of an element and return a completion block with the newly computed height.
+    /// - Parameter completion: A completion block wrapping the newly computed height value.
+    private func computeAgreementHeight(completion: @escaping (CGFloat) -> Void) {
+        self.evaluateJavaScript(
+       """
+        (function GetHeightWidth() {
+        let elem = document.getElementById("agreement");
+      return elem.offsetHeight
+      })();
+      """
+        ) { agreementHeight, error in
+            if let computedHeight = agreementHeight as? CGFloat {
+                completion(computedHeight)
+            }else {
+                completion(0)
+            }
+            
+        }
+    }
+    
+    private func bindContentSize() {
+        // Get the scroll view of the WKWebView
+        // Add an observer to the contentSize property
+        scrollView.addObserver(self, forKeyPath: #keyPath(UIScrollView.contentSize), options: [.new], context: nil)
+    }
+    
+    // An observer method to be notified when the contentSize changes
+    public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(UIScrollView.contentSize) {
+            self.computeAgreementHeight { newAgreementHeight in
+                self.widgetUIDelegate?.didUpdateHeight(self, height: self.updatedHeight + newAgreementHeight)
+                self.invalidateIntrinsicContentSize()
+            }
+        }
+    }
+    
+}
+
